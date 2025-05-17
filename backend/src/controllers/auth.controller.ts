180b51e18d9501491b3ede74892164a35ac4ee83
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2025-05-18 03:54:15 +0700

    [fix] auth get profile error, mismatch object with frontend auth context

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -85,7 +85,7 @@ export class AuthController {
         res.status(401).json({ message: 'Not authenticated' });
         return;
       }
-      res.status(200).json({ user: req.user });
+      res.status(200).json(req.user);
     } catch (error) {
       console.error('[CONTROLLER] Get current user error:', error);
       res.status(500).json({ message: 'Internal server error' });
